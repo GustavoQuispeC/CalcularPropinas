7553aa14b902cddc4dc48e54835c292bf3287a85
Gustavo <gusstavocta@gmail.com>
2024-08-02 23:51:12 -0500

    proyecto terminado

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -8,7 +8,7 @@ const MenuItem = ({ item, addItem }: MenuItemProps) => {
   return (
     <button
       className="flex justify-between w-full p-3 border rounded-md bg-slate-200 border-lime-800 hover:bg-slate-300"
-      onClick={() => addItem(item1)}
+      onClick={() => addItem(item)}
     >
       <p>{item.name}</p>
       <p className="font-black">${item.price}</p>
